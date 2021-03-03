0808deab40755c67cb32d995c84b7e533363a98f
singlewang2020 <1457720646@qq.com>
2021-03-03 14:01:54 +0800

    02 test

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
       <p>祝 java0821 全体同学 拿到自己心仪的offer</p>
 	  <p>
 		<font color='pink'>朦朦</font>
+		<font color='green'>达哥</font>
 	  </p>
    <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
   </div>
