fc0ace4e3241091aa62cb25318798d07ae32983b
singlewang2020 <1457720646@qq.com>
2021-03-03 20:04:40 +0800

    03 test

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,12 @@
 	  <p>
 		<font color='pink'>朦朦</font>
 		<font color='green'>达哥</font>
+		<font color='blue'>锋哥</font>
+		<font color='red'>刚哥</font>
+		<font color='yellow'>辉哥</font>
+		<font color='yellow'>国哥</font>
+                             		<font color='green'>志哥</font>
+		<font color='dark'>金辉哥</font>
 	  </p>
    <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
   </div>
