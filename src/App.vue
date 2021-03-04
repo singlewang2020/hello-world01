59b7550b998e7661d0a1d539d756876b4ca23016
singlewang2020 <1457720646@qq.com>
2021-03-04 14:19:30 +0800

    05 测试自动部署2

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
 		<font color='dark'>金辉哥</font>
 	  </p>
 	  <p>
-		  <font color="#00ffff">翔哥 牛啤！！！</font>
+		  <font color="#00ffff">翔哥 牛啤！！！! !</font>
 	  </P>
    <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
   </div>
